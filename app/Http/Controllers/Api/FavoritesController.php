819036ddfa27c7d5551f1eba1c4e5a6d04e603f2
Alex Ulloa <anders_nielsen_0727@proton.me>
2025-04-19 10:38:53 -0600

    Favoritos

--- app/Http/Controllers/Api/FavoritesController.php
+++ app/Http/Controllers/Api/FavoritesController.php
@@ -3,11 +3,11 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\StoreFavoritesRequest;
 use App\Http\Requests\UpdateFavoritesRequest;
 use App\Models\Favorites;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class FavoritesController extends Controller {
     /**
--- app/Models/Favorites.php
+++ app/Models/Favorites.php
@@ -2,11 +2,20 @@
 
 namespace App\Models;
 
+use Database\Factories\FavoritesFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Passport\HasApiTokens;
 
-class Favorites extends Model
-{
-    /** @use HasFactory<\Database\Factories\FavoritesFactory> */
-    use HasFactory;
+class Favorites extends Model {
+    /** @use HasFactory<FavoritesFactory> */
+    use HasFactory, HasApiTokens;
+
+    protected $table = 'favorites';
+
+    protected $fillable = [
+        'clinic_id',
+        'patient_id',
+    ];
+
 }
