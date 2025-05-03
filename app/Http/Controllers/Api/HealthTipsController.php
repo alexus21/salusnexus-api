1861f44ca165a6642a0aeb5ea83b6e597cc1b487
Alex Ulloa <anders_nielsen_0727@proton.me>
2025-05-03 14:54:14 -0600

    Implementación de deepseek para tips automatizados

--- app/Http/Controllers/Api/HealthTipsController.php
+++ app/Http/Controllers/Api/HealthTipsController.php
@@ -6,62 +6,69 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreHealthTipsRequest;
 use App\Http\Requests\UpdateHealthTipsRequest;
 use App\Models\HealthTips;
+use DeepSeek\DeepSeekClient;
+use Illuminate\Http\JsonResponse;
 
-class HealthTipsController extends Controller
-{
+class HealthTipsController extends Controller {
+    public function aiTipDemo(): JsonResponse {
+        $response = DeepSeekClient::build(env('DEEP_SEEK_API_KEY'))
+            ->query('¿Cual es la mejor manera de prevenir el cáncer?')
+            ->run();
+
+        $decodedResponse = json_decode($response, true);
+        $content = $decodedResponse['choices'][0]['message']['content'] ?? 'No content available';
+
+        return response()->json([
+            'content' => $content,
+        ]);
+    }
+
     /**
      * Display a listing of the resource.
      */
-    public function index()
-    {
+    public function index() {
         //
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
+    public function create() {
         //
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreHealthTipsRequest $request)
-    {
+    public function store(StoreHealthTipsRequest $request) {
         //
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(HealthTips $healthTips)
-    {
+    public function show(HealthTips $healthTips) {
         //
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(HealthTips $healthTips)
-    {
+    public function edit(HealthTips $healthTips) {
         //
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateHealthTipsRequest $request, HealthTips $healthTips)
-    {
+    public function update(UpdateHealthTipsRequest $request, HealthTips $healthTips) {
         //
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(HealthTips $healthTips)
-    {
+    public function destroy(HealthTips $healthTips) {
         //
     }
 }
